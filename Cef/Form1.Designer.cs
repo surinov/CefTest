4a80b66524beb81143e95fdac943add1580ccb23
surinov <54844112+surinov@users.noreply.github.com>
2021-01-09 13:08:21 +0300

    Изменение любого шага JSON

--- Cef/Form1.Designer.cs
+++ Cef/Form1.Designer.cs
@@ -32,12 +32,12 @@ namespace CefTest
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.webBrowser = new CefSharp.WinForms.ChromiumWebBrowser();
-            this.button1 = new System.Windows.Forms.Button();
+            this.runButton = new System.Windows.Forms.Button();
             this.passwordDefBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.adressButton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.debugButton = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.loginDefBox = new System.Windows.Forms.TextBox();
@@ -64,10 +64,22 @@ namespace CefTest
             this.addButton = new System.Windows.Forms.Button();
             this.addBoxDel = new System.Windows.Forms.TextBox();
             this.addBoxCount = new System.Windows.Forms.TextBox();
+            this.tabControlAdd = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBoxAdd = new System.Windows.Forms.ComboBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
+            this.tabControlAdd.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // webBrowser
@@ -79,17 +91,17 @@ namespace CefTest
             this.webBrowser.Size = new System.Drawing.Size(1000, 589);
             this.webBrowser.TabIndex = 0;
             // 
-            // button1
+            // runButton
             // 
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 9.134328F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(19, 127);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 26);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Run";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.runButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.runButton.Font = new System.Drawing.Font("Microsoft YaHei", 9.134328F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.runButton.Location = new System.Drawing.Point(19, 127);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 26);
+            this.runButton.TabIndex = 1;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
             // 
             // passwordDefBox
             // 
@@ -126,21 +138,21 @@ namespace CefTest
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.134328F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(1010, 454);
+            this.label2.Location = new System.Drawing.Point(1013, 454);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(34, 16);
             this.label2.TabIndex = 7;
             this.label2.Text = "Лог:";
             // 
-            // button2
+            // debugButton
             // 
-            this.button2.Location = new System.Drawing.Point(1027, 404);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "Debug";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.debugButton.Location = new System.Drawing.Point(1027, 404);
+            this.debugButton.Name = "debugButton";
+            this.debugButton.Size = new System.Drawing.Size(75, 23);
+            this.debugButton.TabIndex = 8;
+            this.debugButton.Text = "Debug";
+            this.debugButton.UseVisualStyleBackColor = true;
+            this.debugButton.Click += new System.EventHandler(this.debugButton_Click);
             // 
             // label3
             // 
@@ -265,7 +277,7 @@ namespace CefTest
             this.groupBox3.Controls.Add(this.checkAPN);
             this.groupBox3.Controls.Add(this.checkWifi);
             this.groupBox3.Controls.Add(this.checkAuth);
-            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.runButton);
             this.groupBox3.Font = new System.Drawing.Font("Microsoft YaHei", 9.134328F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.groupBox3.Location = new System.Drawing.Point(1008, 242);
             this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
@@ -369,11 +381,11 @@ namespace CefTest
             // 
             // addBoxX
             // 
-            this.addBoxX.Location = new System.Drawing.Point(1144, 279);
+            this.addBoxX.Location = new System.Drawing.Point(6, 43);
             this.addBoxX.Name = "addBoxX";
-            this.addBoxX.Size = new System.Drawing.Size(53, 20);
+            this.addBoxX.Size = new System.Drawing.Size(45, 20);
             this.addBoxX.TabIndex = 23;
-            this.addBoxX.Text = "x";
+            this.addBoxX.Text = "500";
             // 
             // contextMenuStrip1
             // 
@@ -382,65 +394,174 @@ namespace CefTest
             // 
             // addBoxY
             // 
-            this.addBoxY.Location = new System.Drawing.Point(1203, 279);
+            this.addBoxY.Location = new System.Drawing.Point(65, 43);
             this.addBoxY.Name = "addBoxY";
-            this.addBoxY.Size = new System.Drawing.Size(53, 20);
+            this.addBoxY.Size = new System.Drawing.Size(45, 20);
             this.addBoxY.TabIndex = 25;
-            this.addBoxY.Text = "y";
+            this.addBoxY.Text = "500";
             // 
             // addBoxText
             // 
-            this.addBoxText.Location = new System.Drawing.Point(1203, 319);
+            this.addBoxText.Location = new System.Drawing.Point(65, 83);
             this.addBoxText.Name = "addBoxText";
-            this.addBoxText.Size = new System.Drawing.Size(53, 20);
+            this.addBoxText.Size = new System.Drawing.Size(45, 20);
             this.addBoxText.TabIndex = 26;
-            this.addBoxText.Text = "text";
+            this.addBoxText.Text = "helloW";
             // 
             // addBoxDo
             // 
-            this.addBoxDo.Location = new System.Drawing.Point(1144, 319);
+            this.addBoxDo.Location = new System.Drawing.Point(6, 83);
             this.addBoxDo.Name = "addBoxDo";
-            this.addBoxDo.Size = new System.Drawing.Size(53, 20);
+            this.addBoxDo.Size = new System.Drawing.Size(45, 20);
             this.addBoxDo.TabIndex = 27;
-            this.addBoxDo.Text = "do";
+            this.addBoxDo.Text = "click";
             // 
             // addButton
             // 
-            this.addButton.Location = new System.Drawing.Point(1166, 390);
+            this.addButton.Location = new System.Drawing.Point(22, 151);
             this.addButton.Name = "addButton";
             this.addButton.Size = new System.Drawing.Size(75, 23);
             this.addButton.TabIndex = 28;
-            this.addButton.Text = "addButton";
+            this.addButton.Text = "Add";
             this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
             // 
             // addBoxDel
             // 
-            this.addBoxDel.Location = new System.Drawing.Point(1144, 354);
+            this.addBoxDel.Location = new System.Drawing.Point(6, 125);
             this.addBoxDel.Name = "addBoxDel";
-            this.addBoxDel.Size = new System.Drawing.Size(53, 20);
+            this.addBoxDel.Size = new System.Drawing.Size(45, 20);
             this.addBoxDel.TabIndex = 29;
-            this.addBoxDel.Text = "delay";
+            this.addBoxDel.Text = "500";
             // 
             // addBoxCount
             // 
-            this.addBoxCount.Location = new System.Drawing.Point(1203, 354);
+            this.addBoxCount.Location = new System.Drawing.Point(65, 125);
             this.addBoxCount.Name = "addBoxCount";
-            this.addBoxCount.Size = new System.Drawing.Size(53, 20);
+            this.addBoxCount.Size = new System.Drawing.Size(45, 20);
             this.addBoxCount.TabIndex = 30;
-            this.addBoxCount.Text = "count";
+            this.addBoxCount.Text = "14";
+            // 
+            // tabControlAdd
+            // 
+            this.tabControlAdd.Controls.Add(this.tabPage1);
+            this.tabControlAdd.Controls.Add(this.tabPage2);
+            this.tabControlAdd.Location = new System.Drawing.Point(1136, 242);
+            this.tabControlAdd.Name = "tabControlAdd";
+            this.tabControlAdd.SelectedIndex = 0;
+            this.tabControlAdd.Size = new System.Drawing.Size(124, 206);
+            this.tabControlAdd.TabIndex = 31;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label13);
+            this.tabPage1.Controls.Add(this.label12);
+            this.tabPage1.Controls.Add(this.label11);
+            this.tabPage1.Controls.Add(this.label10);
+            this.tabPage1.Controls.Add(this.label9);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.comboBoxAdd);
+            this.tabPage1.Controls.Add(this.addBoxX);
+            this.tabPage1.Controls.Add(this.addBoxY);
+            this.tabPage1.Controls.Add(this.addBoxCount);
+            this.tabPage1.Controls.Add(this.addBoxText);
+            this.tabPage1.Controls.Add(this.addBoxDel);
+            this.tabPage1.Controls.Add(this.addBoxDo);
+            this.tabPage1.Controls.Add(this.addButton);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(116, 180);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "AddPage";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(62, 109);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(35, 13);
+            this.label13.TabIndex = 37;
+            this.label13.Text = "Count";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(6, 109);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(23, 13);
+            this.label12.TabIndex = 36;
+            this.label12.Text = "Del";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(62, 67);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(28, 13);
+            this.label11.TabIndex = 35;
+            this.label11.Text = "Text";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(6, 67);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(21, 13);
+            this.label10.TabIndex = 34;
+            this.label10.Text = "Do";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(62, 27);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(14, 13);
+            this.label9.TabIndex = 33;
+            this.label9.Text = "Y";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 27);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(14, 13);
+            this.label8.TabIndex = 32;
+            this.label8.Text = "X";
+            // 
+            // comboBoxAdd
+            // 
+            this.comboBoxAdd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBoxAdd.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxAdd.FormattingEnabled = true;
+            this.comboBoxAdd.Items.AddRange(new object[] {
+            "login",
+            "wifi",
+            "apn",
+            "remote"});
+            this.comboBoxAdd.Location = new System.Drawing.Point(6, 5);
+            this.comboBoxAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxAdd.Name = "comboBoxAdd";
+            this.comboBoxAdd.Size = new System.Drawing.Size(104, 21);
+            this.comboBoxAdd.TabIndex = 31;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(116, 180);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "P2";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1263, 605);
-            this.Controls.Add(this.addBoxCount);
-            this.Controls.Add(this.addBoxDel);
-            this.Controls.Add(this.addButton);
-            this.Controls.Add(this.addBoxDo);
-            this.Controls.Add(this.addBoxText);
-            this.Controls.Add(this.addBoxY);
-            this.Controls.Add(this.addBoxX);
+            this.Controls.Add(this.tabControlAdd);
             this.Controls.Add(this.logTextBox);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.groupBox4);
@@ -448,7 +569,7 @@ namespace CefTest
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.debugButton);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.webBrowser);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -465,18 +586,21 @@ namespace CefTest
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
             this.groupBox4.ResumeLayout(false);
+            this.tabControlAdd.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button runButton;
         private System.Windows.Forms.TextBox passwordDefBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button adressButton;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button debugButton;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox loginDefBox;
@@ -504,6 +628,16 @@ namespace CefTest
         private System.Windows.Forms.Button addButton;
         private System.Windows.Forms.TextBox addBoxDel;
         private System.Windows.Forms.TextBox addBoxCount;
+        private System.Windows.Forms.TabControl tabControlAdd;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBoxAdd;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
